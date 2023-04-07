84886a3d57dddaee691f6bb8af16229a6fc429e9
kauansousza <kauansouzaesousa@gmail.com>
2023-04-07 19:20:55 -0300

    feat: responsividade

--- src/components/Multiform.tsx
+++ src/components/Multiform.tsx
@@ -21,8 +21,8 @@ const Multiform = () => {
   };
 
   return (
-    <div className="flex items-center justify-center  font-Poppins">
-      <div className="flex flex-col rounded-[2rem] justify-center items-center w-[40rem] h-[25rem] drop-shadow-xl bg-violet-600">
+    <div className="flex items-center justify-center font-Poppins">
+      <div className="flex flex-col rounded-[2rem] justify-center items-center w-11/12 h-[25rem] drop-shadow-xl bg-violet-600">
         <h1 className="font-Poppins text-4xl text-violet-100 font-bold tracking-widest pb-7">
           Formulário
         </h1>
